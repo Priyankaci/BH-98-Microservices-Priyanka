1dc5f686f5f4065a79a74f878e182223de2cd8b0
Priyanka <priyaci2022@gmail.com>
2022-04-28 16:10:33 +0530

    27 April

---
+++ demo-sboot/src/main/java/com/example/demo/DemoSbootApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@ComponentScan("com.example.demo")
+@SpringBootApplication
+@EnableJpaRepositories("com.example.demo")
+public class DemoSbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSbootApplication.class, args);
+	}
+
+}
